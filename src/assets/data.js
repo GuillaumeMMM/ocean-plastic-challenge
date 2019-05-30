cd57e9e6b5f9c69d68790795f69412ddca303870
Guillaume Meigniez <guillaume.meigniez@gmail.com>
2019-05-30 19:53:51 +0800

    first graph

---
+++ src/assets/data.js
@@ -0,0 +1,224 @@
+export const data = [
+    {
+      "year": 1980,
+      "waste_total": 70000000,
+      "waste_mismanadged_%": "100,00",
+      "waste_mismanadged": 70000000
+    },
+    {
+      "year": 1981,
+      "waste_total": 72000000,
+      "waste_mismanadged_%": "98,30",
+      "waste_mismanadged": 70776000
+    },
+    {
+      "year": 1982,
+      "waste_total": 73000000,
+      "waste_mismanadged_%": "97,60",
+      "waste_mismanadged": 71248000
+    },
+    {
+      "year": 1983,
+      "waste_total": 80000000,
+      "waste_mismanadged_%": "96,90",
+      "waste_mismanadged": 77520000
+    },
+    {
+      "year": 1984,
+      "waste_total": 86000000,
+      "waste_mismanadged_%": "96,20",
+      "waste_mismanadged": 82732000
+    },
+    {
+      "year": 1985,
+      "waste_total": 90000000,
+      "waste_mismanadged_%": "95,50",
+      "waste_mismanadged": 85950000
+    },
+    {
+      "year": 1986,
+      "waste_total": 96000000,
+      "waste_mismanadged_%": "94,80",
+      "waste_mismanadged": 91008000
+    },
+    {
+      "year": 1987,
+      "waste_total": 104000000,
+      "waste_mismanadged_%": "94,10",
+      "waste_mismanadged": 97864000
+    },
+    {
+      "year": 1988,
+      "waste_total": 110000000,
+      "waste_mismanadged_%": "92,80",
+      "waste_mismanadged": 102080000
+    },
+    {
+      "year": 1989,
+      "waste_total": 114000000,
+      "waste_mismanadged_%": "91,40",
+      "waste_mismanadged": 104196000
+    },
+    {
+      "year": 1990,
+      "waste_total": 120000000,
+      "waste_mismanadged_%": "90,00",
+      "waste_mismanadged": 108000000
+    },
+    {
+      "year": 1991,
+      "waste_total": 124000000,
+      "waste_mismanadged_%": "88,60",
+      "waste_mismanadged": 109864000
+    },
+    {
+      "year": 1992,
+      "waste_total": 132000000,
+      "waste_mismanadged_%": "87,20",
+      "waste_mismanadged": 115104000
+    },
+    {
+      "year": 1993,
+      "waste_total": 137000000,
+      "waste_mismanadged_%": "85,80",
+      "waste_mismanadged": 117546000
+    },
+    {
+      "year": 1994,
+      "waste_total": 151000000,
+      "waste_mismanadged_%": "84,40",
+      "waste_mismanadged": 127444000
+    },
+    {
+      "year": 1995,
+      "waste_total": 156000000,
+      "waste_mismanadged_%": "83,00",
+      "waste_mismanadged": 129480000
+    },
+    {
+      "year": 1996,
+      "waste_total": 168000000,
+      "waste_mismanadged_%": "81,60",
+      "waste_mismanadged": 137088000
+    },
+    {
+      "year": 1997,
+      "waste_total": 180000000,
+      "waste_mismanadged_%": "80,20",
+      "waste_mismanadged": 144360000
+    },
+    {
+      "year": 1998,
+      "waste_total": 188000000,
+      "waste_mismanadged_%": "78,80",
+      "waste_mismanadged": 148144000
+    },
+    {
+      "year": 1999,
+      "waste_total": 202000000,
+      "waste_mismanadged_%": "77,40",
+      "waste_mismanadged": 156348000
+    },
+    {
+      "year": 2000,
+      "waste_total": 213000000,
+      "waste_mismanadged_%": "76,00",
+      "waste_mismanadged": 161880000
+    },
+    {
+      "year": 2001,
+      "waste_total": 218000000,
+      "waste_mismanadged_%": "74,60",
+      "waste_mismanadged": 162628000
+    },
+    {
+      "year": 2002,
+      "waste_total": 231000000,
+      "waste_mismanadged_%": "73,20",
+      "waste_mismanadged": 169092000
+    },
+    {
+      "year": 2003,
+      "waste_total": 241000000,
+      "waste_mismanadged_%": "71,80",
+      "waste_mismanadged": 173038000
+    },
+    {
+      "year": 2004,
+      "waste_total": 256000000,
+      "waste_mismanadged_%": "70,40",
+      "waste_mismanadged": 180224000
+    },
+    {
+      "year": 2005,
+      "waste_total": 263000000,
+      "waste_mismanadged_%": "69,00",
+      "waste_mismanadged": 181470000
+    },
+    {
+      "year": 2006,
+      "waste_total": 280000000,
+      "waste_mismanadged_%": "67,60",
+      "waste_mismanadged": 189280000
+    },
+    {
+      "year": 2007,
+      "waste_total": 295000000,
+      "waste_mismanadged_%": "66,20",
+      "waste_mismanadged": 195290000
+    },
+    {
+      "year": 2008,
+      "waste_total": 281000000,
+      "waste_mismanadged_%": "64,80",
+      "waste_mismanadged": 182088000
+    },
+    {
+      "year": 2009,
+      "waste_total": 288000000,
+      "waste_mismanadged_%": "63,40",
+      "waste_mismanadged": 182592000
+    },
+    {
+      "year": 2010,
+      "waste_total": 313000000,
+      "waste_mismanadged_%": "62,00",
+      "waste_mismanadged": 194060000
+    },
+    {
+      "year": 2011,
+      "waste_total": 325000000,
+      "waste_mismanadged_%": "60,60",
+      "waste_mismanadged": 196950000
+    },
+    {
+      "year": 2012,
+      "waste_total": 338000000,
+      "waste_mismanadged_%": "59,20",
+      "waste_mismanadged": 200096000
+    },
+    {
+      "year": 2013,
+      "waste_total": 352000000,
+      "waste_mismanadged_%": "57,80",
+      "waste_mismanadged": 203456000
+    },
+    {
+      "year": 2014,
+      "waste_total": 367000000,
+      "waste_mismanadged_%": "56,40",
+      "waste_mismanadged": 206988000
+    },
+    {
+      "year": 2015,
+      "waste_total": 381000000,
+      "waste_mismanadged_%": "55,00",
+      "waste_mismanadged": 209550000
+    },
+    {
+      "year": 2025,
+      "waste_total": "",
+      "waste_mismanadged_%": "",
+      "waste_mismanadged": 235170000
+    }
+  ]
--- src/components/GraphZoom.js
+++ src/components/GraphZoom.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+import { data } from '../assets/data.js';
 
 class GraphZoom extends Component {
 
@@ -26,12 +27,13 @@ class GraphZoom extends Component {
     }
 
     render() {
+
         return (
             <React.Fragment>
                 <div className="graph-container" id="basic-chart" style={{ width: '80vw', height: '60vh' }}>
 
                 </div>
-                <button onClick={this.zoomIn}>Zoom in</button>
+                {/* <button onClick={this.drawHistoryPoints}>Zoom in</button> */}
             </React.Fragment>
         );
     }
@@ -47,19 +49,6 @@ class GraphZoom extends Component {
 
     initGraph = () => {
         const { width, height } = this.state;
-
-        const zoom = () => {
-            let k = d3.event.transform.k;
-            d3.select('.forecast-path').attr("transform", d3.event.transform).style('stroke-width', (0.3 / k) + 'px');
-            d3.select('.now-path').attr("transform", d3.event.transform).style('stroke-width', (0.3 / k) + 'px');
-
-            const new_xScale = d3.event.transform.rescaleX(this.state.x);
-            const new_yScale = d3.event.transform.rescaleY(this.state.y);
-
-            // update axes
-            d3.select('.graph-axis-x').call(d3.axisBottom(new_xScale).tickSize(0).scale(new_xScale));
-            d3.select('.graph-axis-y').call(d3.axisLeft(new_yScale).tickSize(0).scale(new_yScale));
-        }
 
         const svg = d3.select('#basic-chart')
             .append('svg').attr('id', 'main-svg')
@@ -78,36 +67,49 @@ class GraphZoom extends Component {
             .attr('class', 'main-group')
             .attr('transform', 'translate(' + this.relativeWidth(0) + ', ' + this.relativeHeight(0) + ')');
 
+        let totalData = JSON.parse(JSON.stringify(data));
+        totalData.map(year => {
+            year.date = year.year + '-01-01';
+            year.value = year.waste_mismanadged;
+            return year;
+        });
+
         //  Dataset until now with valid dates
-        const untilNowData = JSON.parse(JSON.stringify(this.state.data));
+        const untilNowData = JSON.parse(JSON.stringify(totalData));
+        untilNowData.pop();
         untilNowData.forEach((year, i) => untilNowData[i].date = d3.timeParse("%Y-%m-%d")(year.date));
 
         //  Dataset until the forecast with valid dates
-        const forecastData = JSON.parse(JSON.stringify(this.state.data));
-        forecastData.push(this.state.forecast);
+        const forecastData = JSON.parse(JSON.stringify(totalData));
         forecastData.forEach((year, i) => forecastData[i].date = d3.timeParse("%Y-%m-%d")(year.date));
 
 
         //  Time scale
         const x = d3.scaleTime()
-            .domain(d3.extent(forecastData, function (d) { return d.date; }))
+            .domain(d3.extent(forecastData, function (d, i) { return i === 0 ? d3.timeYear.offset(d.date, -1) : i === forecastData.length - 1 ? d3.timeYear.offset(d.date, 1) : d.date; }))
             .range([this.relativeWidth(margin.left), this.relativeWidth(100 - margin.right)]);
 
         mainGroup.append("g")
             .attr('class', 'graph-axis graph-axis-x')
             .attr("transform", 'translate(0,' + this.relativeHeight(100 - margin.bottom) + ')')
-            .call(d3.axisBottom(x).tickSize(0).ticks(forecastData.length, d3.timeFormat("%Y")));
+            .call(d3.axisBottom(x).tickSize(0).ticks(10, d3.timeFormat("%Y")));
 
+        let yTicks = [];
+        for (let i = 0; i < 6 ; i++) {
+            yTicks.push(i * 50000000);
+        }
+
+        console.log(yTicks)
         //  y scale
         const y = d3.scaleLinear()
-            .domain([0, d3.max(forecastData, function (d) { return d.value; }) + 100])
-            .range([this.relativeHeight(100 - margin.bottom), margin.top]);
+            .domain([0, d3.max(forecastData, function (d) { return d.value; }) + 10000000])
+            .range([this.relativeHeight(100 - margin.bottom), this.relativeHeight(margin.top)]);
 
 
         mainGroup.append("g")
             .attr('class', 'graph-axis graph-axis-y')
             .attr("transform", 'translate(' + this.relativeWidth(margin.left) + ',0)')
-            .call(d3.axisLeft(y).tickSize(0));
+            .call(d3.axisLeft(y).tickSize(0).tickValues(yTicks));
 
         //  clip path graph
         mainGroup.append('clipPath')
@@ -115,30 +117,63 @@ class GraphZoom extends Component {
             .append('rect')
             .attr('x', this.relativeWidth(margin.left) + 0.5)
             .attr('y', this.relativeHeight(margin.top) + 0.5)
-            .attr('width', this.relativeWidth(100) - this.relativeWidth(margin.left) - this.relativeWidth(margin.right))
-            .attr('height', this.relativeHeight(100) - this.relativeHeight(margin.bottom) - this.relativeHeight(margin.top));
+            .attr('width', 0)
+            .attr('height', this.relativeHeight(100) - this.relativeHeight(margin.bottom) - this.relativeHeight(margin.top)).transition().duration(10000).ease(d3.easeLinear)
+            .attr('width', this.relativeWidth(100) - this.relativeWidth(margin.left) - this.relativeWidth(margin.right));
+
+        //  grid clip path graph
+            mainGroup.append('clipPath')
+            .attr('id', 'grid-clip-path')
+            .append('rect')
+            .attr('x', this.relativeWidth(margin.left) + 0.5)
+            .attr('y', this.relativeHeight(margin.top) + 0.5)
+            .attr('height', this.relativeHeight(100) - this.relativeHeight(margin.bottom) - this.relativeHeight(margin.top))
+            .attr('width', this.relativeWidth(100) - this.relativeWidth(margin.left) - this.relativeWidth(margin.right));
 
         //  clip path forecast
         mainGroup.append('clipPath')
             .attr('id', 'forecast-clip-path')
             .append('rect')
-            .attr('x', 0.5 + x(d3.timeParse("%Y-%m-%d")(this.state.data[this.state.data.length - 1].date)))
+            .attr('x', 0.5 + x(d3.timeParse("%Y-%m-%d")(untilNowData[untilNowData.length - 1].date)))
             .attr('y', this.relativeHeight(margin.top) + 0.5)
-            .attr('width', 0.5 + x(forecastData[forecastData.length - 1].date) - x(d3.timeParse("%Y-%m-%d")(this.state.data[this.state.data.length - 1].date)))
+            .attr('width', 0.5 + x(forecastData[forecastData.length - 1].date) - x(d3.timeParse("%Y-%m-%d")(untilNowData[untilNowData.length - 1].date)))
             .attr('height', this.relativeHeight(100) - this.relativeHeight(margin.bottom) - this.relativeHeight(margin.top));
 
         const linesGroup = mainGroup.append('g')
             .attr('clip-path', 'url(#main-clip-path)');
 
+        const gridGroup = mainGroup.append('g').attr('class', 'grid-lines').attr('clip-path', 'url(#grid-clip-path');
+        const gridXGroup = gridGroup.append('g').attr('class', 'grid-x-lines');
+        const gridYGroup = gridGroup.append('g').attr('class', 'grid-y-lines');
+        // gridlines in x axis function
+        function make_x_gridlines() {
+            return d3.axisBottom(x)
+                .ticks(10)
+        }
+        function make_y_gridlines() {
+            return d3.axisLeft(y)
+            .tickValues(yTicks)
+        }
+
+        gridXGroup.attr('transform', 'translate(0, ' + height + ')').call(make_x_gridlines()
+            .tickSize(-height)
+            .tickFormat("")
+        )
+
+        gridYGroup.call(make_y_gridlines()
+            .tickSize(-width)
+            .tickFormat("")
+        )
+
         //  dash line forecast
-        linesGroup.append("path").datum(forecastData)
+        const forecastPath = linesGroup.append("path").datum(forecastData)
             .attr("fill", "none")
             .attr('class', 'forecast-path')
             .attr("stroke", "black")
-            .attr("stroke-width", '0.3px')
+            .attr("stroke-width", '0.4px')
             .attr('clip-path', 'url(#forecast-clip-path)')
             .attr("stroke-dasharray", '1, 1')
-            .attr("d", d3.line().curve(d3.curveMonotoneY)
+            .attr("d", d3.line().curve(d3.curveCardinal)
                 .x(d => x(d.date))
                 .y(function (d) { return y(d.value) })
             );
@@ -148,13 +183,14 @@ class GraphZoom extends Component {
             .attr("fill", "none")
             .attr('class', 'now-path')
             .attr("stroke", "black")
-            .attr("stroke-width", '0.3px')
-            .attr("d", d3.line().curve(d3.curveMonotoneY)
+            .attr("stroke-width", '0.4px')
+            .attr("d", d3.line().curve(d3.curveCardinal)
                 .x(d => x(d.date))
                 .y(function (d) { return y(d.value) })
-            )
+            );
 
-        this.setState({ mainGroup: mainGroup, margin: margin, y: y, x: x, zoom: zoom, svg: svg });
+
+        this.setState({ data: totalData, mainGroup: mainGroup, linesGroup: linesGroup, margin: margin, y: y, x: x, svg: svg }, this.drawHistoryPoints);
     }
 
     //  Takes a % ang returns the right height
@@ -167,41 +203,50 @@ class GraphZoom extends Component {
         return this.state.width < this.state.height ? width : width * this.state.width / this.state.height;
     }
 
-    zoomIn = () => {
-        // let k = 1;
-        // let translateX = - this.state.x(d3.timeParse("%Y-%m-%d")('2024-01-01')) + this.state.x(d3.timeParse("%Y-%m-%d")('2006-01-01'));
-        // let totalWidth = this.state.x(d3.timeParse("%Y-%m-%d")('2025-01-01')) - this.state.x(d3.timeParse("%Y-%m-%d")('2006-01-01'));
-        // let ratio = totalWidth / (this.state.x(d3.timeParse("%Y-%m-%d")('2025-01-01')) - this.state.x(d3.timeParse("%Y-%m-%d")('2024-01-01')));
-        // console.log(ratio)
-        let ratio = 10;
+    drawHistoryPoints = () => {
+        const pointsGroup = this.state.mainGroup.append('g').attr('class', 'points-group');
+        const firstPointGroup = pointsGroup.append('g').attr('transform', 'translate(' + this.state.x(d3.timeParse("%Y-%m-%d")(this.state.data[0].date)) + ', ' + this.state.y(this.state.data[0].value) + ')');
+
+        firstPointGroup.append('circle').attr('class','point-circle').attr('cx', 0).attr('cy', 0).attr('fill', 'black').attr('r', 0).transition().duration(500).delay(1000).attr('r', 1);
+        firstPointGroup.append('rect').attr('x', -0.2).attr('y', -2).attr('width', 0.4).attr('height', 0).attr('fill', 'black').transition().duration(500).delay(1500).attr('height', 18).attr('y', -20);
+        firstPointGroup.append('text').attr('x', 0).attr('y', -18).text('1980').style('font-size', '2.7px').attr('opacity', 0).transition().duration(500).delay(2000).attr('opacity', 1).attr('x', 1);
+        firstPointGroup.append('text').attr('x', 0).attr('y', -15).text('Plastic starts').style('font-size', '2.3px').attr('opacity', 0).transition().duration(500).delay(2500).attr('opacity', 1).attr('x', 1);
+        firstPointGroup.append('text').attr('x', 0).attr('y', -12.5).text('being recycled').style('font-size', '2.3px').attr('opacity', 0).transition().duration(500).delay(2500).attr('opacity', 1).attr('x', 1);
 
 
-        // let centerPointXT = this.relativeWidth(this.state.margin.left) + this.relativeWidth(100 - this.state.margin.left - this.state.margin.right) / 2;
-        // let centerPointYT = this.relativeHeight(this.state.margin.top) + this.relativeHeight(100 - this.state.margin.top - this.state.margin.bottom) / 2;
+        const crisisPointGroup = pointsGroup.append('g').attr('transform', 'translate(' + this.state.x(d3.timeParse("%Y-%m-%d")('2008-01-01')) + ', ' + this.state.y(182088000) + ')');
 
-        // let centerPointX = this.relativeWidth(50);
-        // let centerPointY = this.relativeHeight(50);
+        crisisPointGroup.append('circle').attr('class','point-circle').attr('cx', 0).attr('cy', 0).attr('fill', 'black').attr('r', 0).transition().duration(500).delay(7000).attr('r', 1);
+        crisisPointGroup.append('rect').attr('x', -0.2).attr('y', 2).attr('width', 0.4).attr('height', 0).attr('fill', 'black').transition().duration(500).delay(7500).attr('height', 18)
+        crisisPointGroup.append('text').attr('x', 0).attr('y', 19.5).text('2008').style('font-size', '2.7px').attr('opacity', 0).transition().duration(500).delay(8000).attr('opacity', 1).attr('x', 1);
+        crisisPointGroup.append('text').attr('x', 0).attr('y', 16).text('Economic crisis').style('font-size', '2.3px').attr('opacity', 0).transition().duration(500).delay(8500).attr('opacity', 1).attr('x', 1);
+
+
+        const forecastPointGroup = pointsGroup.append('g').attr('transform', 'translate(' + this.state.x(d3.timeParse("%Y-%m-%d")('2025-01-01')) + ', ' + this.state.y(235170000) + ')');
+
+        forecastPointGroup.append('circle').attr('class','point-circle').attr('cx', 0).attr('cy', 0).attr('fill', 'black').attr('r', 0).transition().duration(500).delay(10000).attr('r', 1);
+        forecastPointGroup.append('rect').attr('x', -0.2).attr('y', 2).attr('width', 0.4).attr('height', 0).attr('fill', 'black').transition().duration(500).delay(10500).attr('height', 18)
+        forecastPointGroup.append('text').attr('x', 0).attr('y', 19.5).text('2025').attr('text-anchor', 'end').style('font-size', '2.7px').attr('opacity', 0).transition().duration(500).delay(11000).attr('opacity', 1).attr('x', -1);
+        forecastPointGroup.append('text').attr('x', 0).attr('y', 16).text('Forecast').attr('text-anchor', 'end').style('font-size', '2.3px').attr('opacity', 0).transition().duration(500).delay(11500).attr('opacity', 1).attr('x', -1);
+    }
+
+    zoomIn = (date) => {
 
         let zoomToPointX = this.state.x(d3.timeParse("%Y-%m-%d")('2025-01-01'));
-        let zoomToPointY = this.state.y(2500);
+        let zoomToPointY = this.state.y(this.state.data[this.state.data.length - 1].value);
 
-        let domainXStart = d3.timeParse("%Y-%m-%d")('2024-12-01');
+        console.log(data[data.length - 1])
+
+        let domainXStart = d3.timeParse("%Y-%m-%d")(date);
         let domainXEnd = d3.timeParse("%Y-%m-%d")('2025-01-01');
         let domainXInitialStart = d3.timeParse("%Y-%m-%d")('2006-01-01');
 
-        let domainYStart = 2400;
-        let domainYEnd = 2500;
+        let domainYStart = this.state.data[this.state.data.length - 1].value - 100;
+        let domainYEnd = this.state.data[this.state.data.length - 1].value;
         let domainYInitialStart = 0;
 
         let ratioX = (this.state.x(domainXEnd) - this.state.x(domainXInitialStart)) / (this.state.x(domainXEnd) - this.state.x(domainXStart));
         let ratioY = (this.state.y(domainYEnd) - this.state.y(domainYInitialStart)) / (this.state.y(domainYEnd) - this.state.y(domainYStart));
-
-        // this.state.svg.append('circle').attr('cx', centerPointX).attr('cy', centerPointY).attr('r', 1).attr('fill', 'red')
-        // this.state.svg.append('circle').attr('cx', centerPointXT).attr('cy', centerPointYT).attr('r', 1).attr('fill', 'orange')
-        // this.state.svg.append('circle').attr('cx', zoomToPointX).attr('cy', zoomToPointY).attr('r', 1).attr('fill', 'green')
-
-        let newYDomainStart = 2500 - (500 / 6);
-        let newYDomainEnd = 2500 - (100 / 6);
 
         // New Time scale
         const newX = d3.scaleTime()
@@ -210,22 +255,52 @@ class GraphZoom extends Component {
 
         // New Y scale
         const newY = d3.scaleLinear()
-            .domain([newYDomainStart, newYDomainEnd])
-            .range([this.relativeHeight(100 - this.state.margin.bottom), this.state.margin.top]);
+            .domain([domainYStart, domainYEnd + ((domainYEnd - domainYStart) / 10)])
+            .range([this.relativeHeight(100 - this.state.margin.bottom), this.relativeHeight(this.state.margin.top)]);
 
-        d3.select('.graph-axis-x').transition().call(d3.axisBottom(newX).tickSize(0));
-        d3.select('.graph-axis-y').transition().call(d3.axisLeft(newY).tickSize(0));
-        // d3.select('.graph-axis-y').call(d3.axisLeft(new_yScale).tickSize(0).scale(new_yScale));
-        // mainGroup.append("g")
-        //     .attr('class', 'graph-axis graph-axis-x')
-        //     .attr("transform", 'translate(0,' + this.relativeHeight(100 - margin.bottom) + ')')
-        //     .call(d3.axisBottom(x).tickSize(0).ticks(forecastData.length, d3.timeFormat("%Y")));
+        d3.select('.graph-axis-x').transition().duration(10000).call(d3.axisBottom(newX).tickSize(0));
+        d3.select('.graph-axis-y').transition().duration(10000).call(d3.axisLeft(newY).tickSize(0));
 
         let translate = "translate(" + (zoomToPointX) + ", " + (zoomToPointY) + ")";
         let translateInverse = "translate(" + (-zoomToPointX) + ", " + (-zoomToPointY) + ")";
         let scale = "scale(" + ratioX + ", " + ratioY + ")";
-        d3.select('.forecast-path').transition().duration(1000).attr("transform", translate + scale + translateInverse).style('stroke-width', (1 / ratioX) + 'px').attr("stroke-dasharray", '0.01, 0.01');
-        d3.select('.now-path').transition().duration(1000).attr("transform", translate + scale + translateInverse).style('stroke-width', (1 / ratioX) + 'px');
+
+        d3.select('.forecast-path').transition().duration(10000).style('stroke-width', (1 / ratioX) + 'px').attr("stroke-dasharray", 'none').attr("transform", translate + scale + translateInverse)
+        // .on('end', this.splitLines.bind(this, this.state.x, this.state.y, newX, newY));
+
+        d3.select('.now-path').transition().duration(10000).attr("transform", translate + scale + translateInverse).style('stroke-width', (1 / ratioX) + 'px');
+    }
+
+    splitLines = (x, y, newX, newY, date) => {
+        const forecastPathClone = d3.select('.forecast-path').clone();
+        let currentBindedData = forecastPathClone.datum();
+        let newForecast = { date: d3.timeParse("%Y-%m-%d")('2025-01-01'), value: 2499.5 };
+        currentBindedData.forEach((val, i) => i !== currentBindedData.length - 1 ? val.value2 = val.value : val.value2 = newForecast.value);
+
+        console.log(currentBindedData)
+        forecastPathClone.datum(currentBindedData).transition().duration(1000)
+            .attr('d', d3.line().curve(d3.curveMonotoneY)
+                .x(d => x(d.date))
+                .y(d => y(d.value2))
+            );
+
+        d3.select('.forecast-path').transition().duration(1000)
+            .style('stroke', 'lightgrey')
+
+        var area = d3.area().curve(d3.curveMonotoneY)
+            .x(d => newX(d.date))
+            .y0(d => newY(d.value))
+            .y1(d => newY(d.value2));
+
+        this.state.linesGroup.append('path')
+            .datum(currentBindedData)
+            .style('fill', 'green')
+            .style('fill-opacity', 0)
+            .style('stroke', 'none')
+            .attr('class', 'area')
+            .attr('d', area)
+            .transition().duration(1000).delay(1000)
+            .style('fill-opacity', 0.05);
     }
 
 
