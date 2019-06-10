2931faa6b0f174a55c38c2a1c356abc9cee28693
Zach <podeng0321@gmail.com>
2019-06-10 18:43:00 +0800

    footer

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -18,7 +18,12 @@ class AppContainer extends Component {
                 <div className='chart-container'>
                     <Graph id={1} showZoomedGraph={this.state.showZoomedGraph}></Graph>
                     <GraphZoom id={2} className="graph-zoom" showZoomedGraph={this.state.showZoomedGraph} clickedFriend={this.state.clickedFriend} clickedFnf={this.state.clickedFnf}></GraphZoom>
-                </div>  
+                </div>
+                <span></span>
+                <div className='footer'>
+                  @2019 Made by <span className='author'>Guillaume</span> and <span onClick={()=> window.open("https://www.linkedin.com/in/zachchang/", "_blank")} className='author'>Zach</span>
+                  <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/prosymbols" title="Prosymbols">Prosymbols</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                </div>
             </React.Fragment>
         );
     }
